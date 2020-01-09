10164043eb0c36fe8a3b48c14e6fd57c571038bc
Nitay Ben-Zvi <nitay.b@cyberghost.ro>
2020-01-09 14:13:54 +0200

    fix paths

--- app.js
+++ app.js
@@ -71,10 +71,14 @@ app.use(mws.versioning(__dirname + '/assets'));
 
 
 app.use('/*/img/', express.static(__dirname + '/assets/img', {maxAge: cacheTime }));
+app.use('/img/', express.static(__dirname + '/assets/img', {maxAge: cacheTime }));
 app.use('css/img', express.static(__dirname + '/assets/img', {maxAge: cacheTime }));
 app.use('/*/css/', express.static(__dirname + '/assets/css', {maxAge: cacheTime }));
+app.use('/css/', express.static(__dirname + '/assets/css', {maxAge: cacheTime }));
 app.use('/*/js/', express.static(__dirname + '/assets/js', {maxAge: cacheTime }));
+app.use('/js/', express.static(__dirname + '/assets/js', {maxAge: cacheTime }));
 app.use('/*/lib/', express.static(__dirname + '/assets/lib', {maxAge: cacheTime }));
+app.use('/lib/', express.static(__dirname + '/assets/lib', {maxAge: cacheTime }));
 app.use('/offer', express.static(__dirname + '/assets', {maxAge: cacheTime }));
 app.use('/pages/offer', express.static(__dirname + '/assets', {maxAge: cacheTime }));
 app.use(express.static(__dirname + '/assets', {maxAge: cacheTime }));

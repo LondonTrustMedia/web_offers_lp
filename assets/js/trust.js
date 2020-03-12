a0732974d268fc786248acbcb7b64884a057eab6
Guy Tagger <guy.r@kape.com>
2020-03-12 16:48:20 +0200

    finish qa

--- assets/js/trust.js
+++ assets/js/trust.js
@@ -8,7 +8,7 @@ $(function() {
         let h = window.innerHeight/2;
         let _mouseX = e.clientX;
         let _mouseY = e.clientY;
-        let move = `translate( ${-(_mouseX - w) * 0.002}px, ${-(_mouseY - h) * 0.002}px)`;
+        let move = `translate( ${-(_mouseX - w) * 0.004}px, ${-(_mouseY - h) * 0.004}px)`;
         pra.css("transform",move)
     }
     if($(document).innerWidth < 1200) elem.off("mousemove", parallax);
